19a6d266bbbfc0f71b469a445eb1a4a41ffd470c
Saad Siddiq <siddiq.saad@gmail.com>
2022-07-26 02:20:54 -0500

    added tracking opponent player

--- Game.rb
+++ Game.rb
@@ -1,16 +1,22 @@
 class Game
   def initialize(players)
     @players = players
-    @player_index = 0
-    @current_player = @players[@player_index]
+    @player_index = 1
+    @current_player = @players[1]
+    @other_player = @players[0]
     @over = false
   end
 
-  def next_player
+  def player_change
+    @other_player = @players[@player_index]
     @player_index = (@player_index == 0) ? 1 : 0
     @current_player = @players[@player_index]
   end
 
-  attr_reader :current_player
+  def scoreboard
+    puts "P1: #{@players[0].lives}/3 vs P2: #{@players[1].lives}/3"
+  end
+
+  attr_reader :current_player, :other_player
   attr_accessor :over
 end
