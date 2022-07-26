623c54cf651040a10dc1b407a506b365d02ed877
Saad Siddiq <siddiq.saad@gmail.com>
2022-07-26 01:23:48 -0500

    changed entire question to smaller prompt

--- Question.rb
+++ Question.rb
@@ -2,9 +2,9 @@ class Question
   def initialize
     firstNum = rand(1..20)
     secondNum = rand(1..20)
-    @question = "What does #{firstNum} plus #{secondNum} equal?"
+    @prompt = "#{firstNum} plus #{secondNum}"
     @answer = firstNum + secondNum
   end
 
-  attr_reader :question, :answer
+  attr_reader :prompt, :answer
 end
